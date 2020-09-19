1a073f34442c3029232a6c7e8def28f82bae4e12
NicoMN <nicolasmendes.19@hotmail.com>
2020-09-19 15:35:25 -0300

    Added offline mode

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
+const cacheName ='app-shell-v1';
+const assetsToCache = [
+    'offline.html'
+];
+
+async function cacheStaticAssets () {
+    const cache = await caches.open(cacheName);   
+    return cache.addAll(assetsToCache);
+}
+
 self.addEventListener('install', (event) => {
 console.log('[Service worker] Installing service worker...', event);
+
+event.waitUntil(cacheStaticAssets());
 self.skipWaiting();
 });
 
@@ -8,6 +20,16 @@ self.addEventListener('activate', event => {
     return self.clients.claim();
 });
 
+async function networkFirst(request) {
+    try {
+        return await fetch(request);
+    } catch(error) {
+        const cache = await caches.open(cacheName);
+        return cache.match('offline.html');
+    }
+}
+
 self.addEventListener('fetch', event => {
     console.log('[Service worker] Fetch event: ' + event.request.url);        
+    event.respondWith(networkFirst(event.request));
 });
